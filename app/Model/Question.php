f4f5443600e225d05faebdd79f2761ee99aaffd1
apth-svg <aungphone289@gmail.com>
2020-01-18 07:29:26 -0500

    CRUD for Question along with Question transform

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,6 +9,12 @@ use App\Model\Category;
 
 class Question extends Model
 {
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+   // protected $fillable=['title','slug','body','category_id','user_id'];
+    protected $guarded=[];
+    
     public function user(){
         return $this->belongsTo(User::class);
     }
@@ -17,5 +23,8 @@ class Question extends Model
     }
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
     }
 }
